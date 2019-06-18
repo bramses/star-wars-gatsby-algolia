ab9a1b8b5097a4bf5a50737222d4a9cf25cb696f
Bram Adams <bram@algolia.com>
2019-06-18 14:19:19 -0400

    - moved left column - edited css for subtitles

--- src/components/Character.js
+++ src/components/Character.js
@@ -6,16 +6,10 @@ const Character = ({ hit }) => (
         <h2 className="star-wars">
             <Highlight hit={hit} attribute="name" />
         </h2>
-        <span className="star-wars-subtitle">Height: {hit.height ? `${hit.height}cm` : 'NO HEIGHT FOUND'}</span>
-        <br />
-        <br />
+        <span className="star-wars-subtitle">Height: {hit.height ? `${hit.height}cm` : 'NO HEIGHT FOUND'}</span>     
         <span className="star-wars-subtitle">Birth Year: {hit.birthYear ? `${hit.birthYear} BBY` : 'NO BIRTH YEAR FOUND'}</span>
-        <br />
-        <br />
-        <span className="star-wars-subtitle">Species: {hit.species ? hit.species[0] ? hit.species[0]["name"] ? `${hit.species[0]["name"]}` : 'NO SPECIES FOUND' : 'NO SPECIES FOUND' : 'NO SPECIES FOUND'}</span>
-        <br />
-        <br />
-        <span className="star-wars-subtitle">Homeworld: {hit.homeworld ? hit.homeworld.name ? `${hit.homeworld.name}` : 'NO HOMEWORLD FOUND' : 'NO HOMEWORLD FOUND'}</span>
+        <span className="star-wars-subtitle">Species:  <Highlight hit={hit} attribute="species[0].name" /></span>
+        <span className="star-wars-subtitle">Homeworld: <Highlight hit={hit} attribute="homeworld.name" /></span>
     </div>
   )
   
